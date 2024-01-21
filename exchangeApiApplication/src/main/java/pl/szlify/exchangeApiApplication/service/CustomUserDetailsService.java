75bcb226c2d02f801f08ce338ea07df27f7e568b
adixks <adrianks@op.pl>
2024-01-22 00:03:46 +0100

    Add CustomUserDetailsService

---
+++ exchangeApiApplication/src/main/java/pl/szlify/exchangeApiApplication/service/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package pl.szlify.exchangeApiApplication.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.szlify.exchangeApiApplication.model.entity.UserEntity;
+import pl.szlify.exchangeApiApplication.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return new User(userEntity.getUsername(), userEntity.getPassword(), userEntity.getAuthorities());
+    }
+
+    @Transactional
+    public UserEntity loadUserEntityByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+    }
+}
